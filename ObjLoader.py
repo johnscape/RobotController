cf6fc1b4c6d239a23b3a2fc82f962a221c0fb9b9
Baczay Attila <johnscape96@gmail.com>
2020-04-01 17:12:24 +0200

    Removed old collision detection and added comments

--- ObjLoader.py
+++ ObjLoader.py
@@ -1,122 +1,152 @@
 import copy
 
+#IMPORTANT NOTE: In this project, I'll be using the Z-up setup, meaning, that Y will be depth and Z will be the height value
+
 class Verticle2D:
+    """
+    A class used to represent a 2D verticle
+
+    Attributes
+    ----------
+    X : float
+        The X coordinate of the verticle
+    Y : float
+        The Y coordinate of the verticle
+    """
     def __init__(self, x = 0, y = 0):
         self.X = x
         self.Y = y
 
 class Verticle:
+    """
+    A class used to represent a 3D verticle
+
+    Attributes
+    ----------
+    X : float
+        The X coordinate of the verticle
+    Y : float
+        The Y coordinate of the verticle
+    Z : float
+        The Z coordinate of the verticle
+
+    Methods
+    -------
+    GetValue(coordinate)
+        Returns with the selected coordinate
+    """
     def __init__(self, x: float, y: float, z: float):
         self.X = x
         self.Y = y
         self.Z = z
 
+    def GetValue(self, coordinate: int) -> float:
+        """
+        Returns with the selected coordinate
+        If the value is out of range, returns 0
+
+        Parameters
+        ----------
+        coordinate : int
+            The coordinate to return
+        """
+        if coordinate == 0: return self.X
+        elif coordinate == 1: return self.Y
+        elif coordinate == 2: return self.Z
+        return 0
+
 class Face:
+    """
+    A class used to represent an face created by 3 vertices
+
+    Attributes
+    ----------
+    Verticle1 : Verticle
+        The first verticle which creates the face
+    Verticle1 : Verticle
+        The second verticle which creates the face
+    Verticle1 : Verticle
+        The third verticle which creates the face
+
+    Methods
+    -------
+    CollisionCheck(line_start, line_end)
+        Returns a True if the 3D line intersects/collides with the face, False otherwise.
+    """
     def __init__(self, v1: Verticle, v2: Verticle, v3: Verticle):
         self.Verticle1 = v1
         self.Verticle2 = v2
         self.Verticle3 = v3
 
-class FaceObject:
-    def __init__(self, face: Face):
-        self.Point1 = copy.deepcopy(face.Verticle1)
-        self.Point2 = copy.deepcopy(face.Verticle2)
-        self.Point3 = copy.deepcopy(face.Verticle3)
-
-        # now we create a box, which surround the face for faster collision detection
-        self.BoxPoints = []
-        self.__Temp = []
-
-        # find limits of x, y and z
-        self.__FindLimitValues()
-
-        # setting bounding box points
-        self.BoxPoints.append(Verticle(self.__Temp[3], self.__Temp[1], self.__Temp[2])) # top front left - smallest x, largest y, largest z
-        self.BoxPoints.append(Verticle(self.__Temp[0], self.__Temp[1], self.__Temp[2])) # top front right - largest x, largest y, largest z
-        self.BoxPoints.append(Verticle(self.__Temp[3], self.__Temp[4], self.__Temp[2])) # top back left - smallest x, smallest y, largest z
-        self.BoxPoints.append(Verticle(self.__Temp[0], self.__Temp[4], self.__Temp[2])) # top back right - largest x, smallest y, largest z
-
-        self.BoxPoints.append(Verticle(self.__Temp[3], self.__Temp[1], self.__Temp[2])) # bottom front left - smallest x, largest y, smallest z
-        self.BoxPoints.append(Verticle(self.__Temp[0], self.__Temp[1], self.__Temp[2])) # bottom front right - largest x, largest y, smallest z
-        self.BoxPoints.append(Verticle(self.__Temp[3], self.__Temp[4], self.__Temp[2])) # bottom back left - smallest x, smallest y, smallest z
-        self.BoxPoints.append(Verticle(self.__Temp[0], self.__Temp[4], self.__Temp[2])) # bottom back right - largest x, smallest y, smallest z
-    
-    def __FindLimitValues(self):
-        lx = self.Point1.X
-        if self.Point2.X > lx:
-            lx = self.Point2.X
-        if self.Point3.X > lx:
-            self.Point3.X
+    def CollisionCheck(self, line_start: Verticle, line_end: Verticle) -> bool:
+        """
+        Returns a True if the 3D line intersects/collides with the face, False otherwise.
         
-        sx = self.Point1.X
-        if self.Point2.X < sx:
-            sx = self.Point2.X
-        if self.Point3.X < sx:
-            sx = self.Point3.X
+        Parameters
+        ----------
+        line_start : Verticle
+            The starting point of the 3D line
+        line_end : Verticle
+            The ending point of the 3D line
+        """
+        front = self.__PartCheck(line_start, line_end, 0, 2)
+        side = self.__PartCheck(line_start, line_end, 1, 2)
+        top = self.__PartCheck(line_start, line_end, 0, 1)
 
-        ly = self.Point1.Y
-        if self.Point2.Y > ly:
-            ly = self.Point2.Y
-        if self.Point3.Y > ly:
-            self.Point3.Y
+        return (front and side) or (side and top) or (front and top)
 
-        sy = self.Point1.Y
-        if self.Point2.Y < sy:
-            sy = self.Point2.Y
-        if self.Point3.Y < sy:
-            sy = self.Point3.Y
-
-        lz = self.Point1.Z
-        if self.Point2.Z > lz:
-            lz = self.Point2.Z
-        if self.Point3.Z > lz:
-            self.Point3.Z
-
-        sz = self.Point1.Z
-        if self.Point2.Z < sz:
-            sz = self.Point2.Z
-        if self.Point3.Z < sz:
-            sz = self.Point3.Z
+    def __PartCheck(self, line_start: Verticle, line_end: Verticle, x_dimension: int, y_dimension: int) -> bool:
+        """
+        Returns a True if the 3D line intersects/collides with the face from a specific view, False otherwise.
         
-        self.__Temp.append(lx, ly, lz, sx, sy, sz)
+        Parameters
+        ----------
+        line_start : Verticle
+            The starting point of the 3D line
+        line_end : Verticle
+            The ending point of the 3D line
+        x_dimension : int
+            Selects a dimension from 3D to use in 2D as X (i.e. 1 means that the 3D Y value will be used as the 2D X)
+        y_dimension : int
+            Selects a dimension from 3D to use in 2D as Y (i.e. 2 means that the 3D Z value will be used as the 2D Y)
+        """
+        line_start_view = Verticle2D(line_start.GetValue(x_dimension), line_start.GetValue(y_dimension))
+        line_end_view = Verticle2D(line_end.GetValue(x_dimension), line_end.GetValue(y_dimension))
 
-    def BoxCollision(self, line_start: Verticle, line_end: Verticle):
-        # front view
-        start = Verticle2D(line_start.X, line_start.Z)
-        end = Verticle2D(line_end.X, line_end.Z)
-        p1 = Verticle2D(self.BoxPoints[0].X, self.BoxPoints[0].Z)
-        p2 = Verticle2D(self.BoxPoints[1].X, self.BoxPoints[1].Z)
-        p3 = Verticle2D(self.BoxPoints[4].X, self.BoxPoints[4].Z)
-        p4 = Verticle2D(self.BoxPoints[5].X, self.BoxPoints[5].Z)
-        front = self.RectCollision(start, end, p1, p2, p3, p4)
-        # side view
-        start = Verticle2D(line_start.Y, line_start.Z)
-        end = Verticle2D(line_end.Y, line_end.Z)
-        p1 = Verticle2D(self.BoxPoints[0].Y, self.BoxPoints[0].Z)
-        p2 = Verticle2D(self.BoxPoints[2].Y, self.BoxPoints[2].Z)
-        p3 = Verticle2D(self.BoxPoints[4].Y, self.BoxPoints[4].Z)
-        p4 = Verticle2D(self.BoxPoints[6].Y, self.BoxPoints[6].Z)
-        side = self.RectCollision(start, end, p1, p2, p3, p4)
-        # top view
-        start = Verticle2D(line_start.X, line_start.Y)
-        end = Verticle2D(line_end.X, line_end.Y)
-        p1 = Verticle2D(self.BoxPoints[2].X, self.BoxPoints[2].Y)
-        p2 = Verticle2D(self.BoxPoints[4].X, self.BoxPoints[4].Y)
-        p3 = Verticle2D(self.BoxPoints[0].X, self.BoxPoints[0].Y)
-        p4 = Verticle2D(self.BoxPoints[3].X, self.BoxPoints[3].Y)
-        top = self.RectCollision(start, end, p1, p2, p3, p4)
+        #v1 and v2
+        collider_start = Verticle2D(self.Verticle1.GetValue(x_dimension), self.Verticle1.GetValue(y_dimension))
+        collider_end = Verticle2D(self.Verticle2.GetValue(x_dimension), self.Verticle2.GetValue(y_dimension))
+        if self.LineCollision(line_start_view, line_end_view, collider_start, collider_end):
+            return True
+        # v2 and v3
+        collider_start = Verticle2D(self.Verticle2.GetValue(x_dimension), self.Verticle2.GetValue(y_dimension))
+        collider_end = Verticle2D(self.Verticle3.GetValue(x_dimension), self.Verticle3.GetValue(y_dimension))
+        if self.LineCollision(line_start_view, line_end_view, collider_start, collider_end):
+            return True
 
-        return (front or side or top)
-    
-    def RectCollision(self, line_start: Verticle2D, line_end: Verticle2D, point1: Verticle2D, point2: Verticle2D, point3: Verticle2D, point4: Verticle2D):
-        left = self.LineCollision(line_start, line_end, point1, point3)
-        right = self.LineCollision(line_start, line_end, point2, point4)
-        top = self.LineCollision(line_start, line_end, point1, point2)
-        bottom = self.LineCollision(line_start, line_end, point3, point4)
-        return (left or right or top or bottom)
+        # v3 and v1
+        collider_start = Verticle2D(self.Verticle3.GetValue(x_dimension), self.Verticle3.GetValue(y_dimension))
+        collider_end = Verticle2D(self.Verticle1.GetValue(x_dimension), self.Verticle1.GetValue(y_dimension))
+        return self.LineCollision(line_start_view, line_end_view, collider_start, collider_end)
+        
+
 
     @staticmethod
-    def LineCollision(line_a_start: Verticle2D, line_a_end: Verticle2D, line_b_start: Verticle2D, line_b_end: Verticle2D):
+    def LineCollision(line_a_start: Verticle2D, line_a_end: Verticle2D, line_b_start: Verticle2D, line_b_end: Verticle2D) -> bool:
+        """
+        Returns a True if the two 2D line intersects/collides, False otherwise.
+
+        Parameters
+        ----------
+        line_a_start : Verticle2D
+            The starting point of the first 2D line
+        line_a_end : Verticle2D
+            The ending point of the first 2D line
+        line_b_start : Verticle2D
+            The starting point of the second 2D line
+        line_b_end : Verticle2D
+            The ending point of the second 2D line
+        """
         uA = ((line_b_end.x-line_b_start.x)*(line_a_start.y-line_b_start.y) - (line_b_end.y-line_b_start.y)*(line_a_start.x-line_b_start.x)) / ((line_b_end.y-line_b_start.y)*(line_a_end.x-line_a_start.x) - (line_b_end.x-line_b_start.x)*(line_a_end.y-line_a_start.y))
         uB = ((line_a_end.x-line_a_start.x)*(line_a_start.y-line_b_start.y) - (line_a_end.y-line_a_start.y)*(line_a_start.x-line_b_start.x)) / ((line_b_end.y-line_b_start.y)*(line_a_end.x-line_a_start.x) - (line_b_end.x-line_b_start.x)*(line_a_end.y-line_a_start.y))
         return (uA >= 0 and uA <= 1 and uB >= 0 and uB <= 1)
@@ -127,7 +157,6 @@ class ObjFile:
         self.FileName = file
         self.Vertices = []
         self.Faces = []
-        self.GeneratedFaces = []
 
         if file is not None:
             self.LoadFile(self.FileName)
@@ -136,7 +165,6 @@ class ObjFile:
         if self.FileName is None:
             self.FileName = file
         self.__ReadFile()
-        self.__GenerateData()
         
 
     def __ReadFile(self):
@@ -158,9 +186,3 @@ class ObjFile:
                         break
                 line = reader.readline()
                 current_line = 1
-    
-    def __GenerateData(self):
-        for f in self.Faces:
-            self.GeneratedFaces.append(FaceObject(f))
-        self.Faces = []
-        self.Vertices = []
